6e1004c81c0646f812234b8fd9352cb9f4de453d
lucassm19 <124300806+lucassm19@users.noreply.github.com>
2023-09-13 21:33:17 -0300

    Estilos e Router

---
+++ estilos/src/StudentList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StudentList.scss";
+const StudentList = (props) => {
+    const { name, registration, classId, year } = props;
+    return (
+        <ul className="list">
+            <li className="details">Nome : {name}</li>
+            <li className="details">Matrícula: {registration}</li>
+            <li className="details">Turma: {classId}</li>
+            <li className="details">Ano : {year}</li>
+        </ul>
+    );
+};
+export default StudentList;
